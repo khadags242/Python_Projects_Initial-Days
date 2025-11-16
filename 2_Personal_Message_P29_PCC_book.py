7ae92e7b6e865f5d1cb83e17aed88a80d4cef00a
khadags242 <khadags242@gmail.com>
2025-11-16 13:24:50 +0530

    1st commit

---
+++ 2_Personal_Message_P29_PCC_book.py
@@ -0,0 +1,3 @@
+## Ask is to store a person's name in a variable and print a message to that person. The message should be simple, such as "Hello Eric, would you like to learn Python today?"
+person_name = input("Please enter the person's name: ")
+print(f"{person_name}, would you like learn Python today?")
