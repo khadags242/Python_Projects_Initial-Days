7ed4f3d6771f03f6c763a5a309a995a67da7e2d1
khadags242 <khadags242@gmail.com>
2025-11-23 18:16:53 +0530

    Initial commit

---
+++ 14_odd_numbers_P64_PCC_book.py
@@ -0,0 +1,10 @@
+# Use the range function to make a list of the odd numbers from 1 to 20. Use a for loop to print each number
+
+odds=[]
+
+for value in range(1,20):
+    odd=value
+    odds.append(odd)
+    
+print(odds)
+    
---
+++ 16_cubes_P64_PCC_book.py
@@ -0,0 +1,4 @@
+numbers=range(1,11)**3
+
+for number in numbers:
+    print(number)
